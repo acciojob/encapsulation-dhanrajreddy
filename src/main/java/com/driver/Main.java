d5c64e3a58f5525cb54343870d99026edce929a5
dhanrajreddy <dhanrajkutwade7@gmail.com>
2023-12-19 22:22:57 +0530

    code error mannaged

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     RWOnly e=new RWOnly();
-    e.seter(100);
-    int ans=e.geter();
+    e.setter(100);
+    int ans=e.getter();
     System.out.println(ans);
   }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
-public class RWOnly {
-    
+class RWOnly {
+    private int name;
+    void setter(int input){
+        name =input;
+    }
+
+    int getter(){
+        return name;
+    }
 }
