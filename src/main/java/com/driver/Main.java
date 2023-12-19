4ec83ea32bb27cbeb2d5210bba233d9818b8451f
dhanrajreddy <dhanrajkutwade7@gmail.com>
2023-12-19 22:16:25 +0530

    code written

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly e=new RWOnly();
+    e.seter(100);
+    int ans=e.geter();
+    System.out.println(ans);
+  }
 }
